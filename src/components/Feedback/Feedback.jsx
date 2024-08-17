9b0029bd116c3eeb3db0aea107dacfb07a0e24d6
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-08-17 23:18:25 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,22 +1,11 @@
 export default function Feedback({ value, totalFeedback, positiveFeedback }) {
   return (
     <>
-      <p>
-        <b>Good:</b> {value["good"]}
-      </p>
-      <p>
-        <b>Neutral:</b> {value["neutral"]}
-      </p>
-      <p>
-        <b>Bad:</b> {value["bad"]}
-      </p>
-
-      <p>
-        <b>Total:</b> {totalFeedback}
-      </p>
-      <p>
-        <b>Positive:</b> {positiveFeedback}%
-      </p>
+      <p>Good: {value["good"]}</p>
+      <p>Neutral: {value["neutral"]}</p>
+      <p>Bad: {value["bad"]}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedback}%</p>
     </>
   );
 }
